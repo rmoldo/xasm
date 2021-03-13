2d70b275fdd2eaa7541c6b2eb987498a8a4a6d75
Radu Moldovan <radu.moldovan@ulbsibiu.ro>
2021-03-13 17:40:28 +0200

    Added defs header file
    defs.h contains typedefs of unsigned data types

---
+++ src/defs.h
@@ -0,0 +1,12 @@
+//
+// Created by moldo on 13/03/2021.
+//
+
+#ifndef XASM_DEFS_H
+#define XASM_DEFS_H
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+#endif //XASM_DEFS_H
